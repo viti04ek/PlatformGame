277f303b0327242cc4830d81438494fa23a0edd7
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-03-18 02:21:53 +0200

    Squeezing the player
    ADDED:
    - the ability to squeeze the player when you click on left ctrl, S or down arrow
    - jumping player squeeze
    
    CHANGED:
    - PlayerMove script

--- PlatformGame/Assets/PlayerMove.cs
+++ PlatformGame/Assets/PlayerMove.cs
@@ -12,11 +12,19 @@ public class PlayerMove : MonoBehaviour
 
     public float MaxSpeed;
 
+    public Transform ColliderTransform;
+
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && Grounded)
             Rigidbody.AddForce(0, JumpSpeed, 0, ForceMode.VelocityChange);
+
+
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || !Grounded)
+            ColliderTransform.localScale = Vector3.Lerp(ColliderTransform.localScale, new Vector3(1, 0.5f, 1), Time.deltaTime * 15);
+        else
+            ColliderTransform.localScale = Vector3.Lerp(ColliderTransform.localScale, new Vector3(1, 1, 1), Time.deltaTime * 15);
     }
 
 
@@ -30,7 +38,9 @@ public class PlayerMove : MonoBehaviour
         if ( (Rigidbody.velocity.x > MaxSpeed && Input.GetAxis("Horizontal") > 0) || (Rigidbody.velocity.x < -MaxSpeed && Input.GetAxis("Horizontal") < 0) )
             speedMultiplier = 0;
 
+
         Rigidbody.AddForce(Input.GetAxis("Horizontal") * MoveSpeed * speedMultiplier, 0, 0, ForceMode.VelocityChange);
+
 
         if (Grounded)
             Rigidbody.AddForce(-Rigidbody.velocity.x * Friction, 0, 0, ForceMode.VelocityChange);
