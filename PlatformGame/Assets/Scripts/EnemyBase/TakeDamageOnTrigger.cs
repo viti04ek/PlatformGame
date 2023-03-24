91381228cd96d4f5a0f76315bf09ad2f15549ae5
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-03-24 02:06:07 +0200

    Make and take damage on trigger by rabbit
    ADDED:
    - script MakeDamageOnTrigger
    - script TakeDamageOnTrigger

---
+++ PlatformGame/Assets/Scripts/EnemyBase/TakeDamageOnTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamageOnTrigger : MonoBehaviour
+{
+    public EnemyHealth EnemyHealth;
+
+    public bool DieOnAnyCollision;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody && other.attachedRigidbody.GetComponent<Bullet>())
+            EnemyHealth.TakeDamage(1);
+
+        if (DieOnAnyCollision)
+            EnemyHealth.TakeDamage(10000);
+    }
+}
