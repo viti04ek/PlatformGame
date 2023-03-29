dd3b1964639290e549d91ce9d2518c5ea4996885
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-03-29 03:00:41 +0300

    Activate enemies near the player
    ADDED:
    - script ActivateByDistance
    - script Activator

---
+++ PlatformGame/Assets/Scripts/ActivateByDistance/ActivateByDistance.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateByDistance : MonoBehaviour
+{
+    public float DistanceToActivate = 20;
+    private bool _isActive = true;
+    private Activator _activator;
+
+
+    private void Start()
+    {
+        _activator = FindObjectOfType<Activator>();
+
+        _activator.ObjectsToActivate.Add(this);
+    }
+
+
+    public void CheckDistance(Vector3 playerPosition)
+    {
+        float distance = Vector3.Distance(transform.position, playerPosition);
+
+        if (distance < DistanceToActivate && !_isActive)
+            Activate();
+        else if (distance > DistanceToActivate + 2f && _isActive)
+            Deactivate();
+    }
+
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+
+        _isActive = true;
+    }
+
+
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+
+        _isActive = false;
+    }
+
+
+    private void OnDestroy()
+    {
+        _activator.ObjectsToActivate.Remove(this);
+    }
+}
