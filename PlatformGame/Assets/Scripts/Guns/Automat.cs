ff75caa33b6dad5f756bd08d695f82ef34f3a100
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-03-30 03:30:00 +0300

    Inheritance
    ADDED:
    - script Pistol
    - script Automat

---
+++ PlatformGame/Assets/Scripts/Guns/Automat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Automat : Gun
+{
+    [Header("Automat")]
+    public int NumberOfBullets;
+    public Text BulletsText;
+}
